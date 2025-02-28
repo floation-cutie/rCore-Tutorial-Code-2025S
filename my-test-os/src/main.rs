cfe783f124d7090860600d95ccd576da73d9fc39
floation_cutie <wzxFloation_cutie@bupt.edu.cn>
2025-02-28 03:48:23 +0000

    Add:finish test-os println and shutdown function

--- my-test-os/src/main.rs
+++ my-test-os/src/main.rs
@@ -1,16 +1,20 @@
 #![no_main]
 #![no_std]
+#![feature(panic_info_message)]
+#[macro_use]
+mod console;
 mod lang_items;
 mod sbi;
+mod logging;
 
-use core::fmt::{self, Write};
+//use core::fmt::{self, Write};
 use crate::sbi::shutdown;
 use core::arch::global_asm;
-
+use log::*;
 global_asm!(include_str!("entry.asm"));
 
 const SYSCALL_EXIT: usize = 93;
-const SYSCALL_WRITE: usize = 64;
+// const SYSCALL_WRITE: usize = 64;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret;
@@ -30,6 +34,7 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
+/*
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
   syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
@@ -61,6 +66,7 @@ macro_rules! println {
     }
 }
 
+*/
 
 /*
 //#[no_mangle]
@@ -72,6 +78,53 @@ pub extern "C" fn _start() {
 
 */
 
-fn main() {
-    //    println!("Hello, world!");
+#[no_mangle]
+pub fn rust_main() -> ! {
+
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
+    clear_bss();
+    logging::init();
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    shutdown();
 }
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+
